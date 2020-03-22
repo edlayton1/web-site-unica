cb0ecf8565c041e083f79852f5a7d22f1aa9db8f
albertogfm <albertogarciafigueroam@gmail.com>
2020-03-21 22:26:35 -0600

    HEADER UPDATE

--- src/public/templates/Header.js
+++ src/public/templates/Header.js
@@ -8,16 +8,16 @@ const Header = () => {
     
             <nav>
                 <ul>
-                    <li><a href="/"><span class="home_icon"></span>Home</a></li>
-                    <li><a href="#/notes/"><span class="note_icon"></span>Notas</a></li>
+                    <li><a href="/"><span class="news_icon"></span>Noticias</a></li>
+                    <li><a href="#/notes/"><span class="top_icon"></span>Top 10</a></li>
                     <li class="submenu">
-                        <a href="#/projects/"><span class="project_icon"></span>Proyectos<span class="caret icon-arrow-down6"></span></a>
+                        <a href="#/projects/"><span class="console_icon"></span>Consolas<span class="caret icon-arrow-down6"></span></a>
                         <ul class="children">
                             <li><a href="#/project2020/">Proyectos 2020 <span class="icon-dot"></span></a></li>
                             <li><a href="#/project2019/">Proyectos 2019 <span class="icon-dot"></span></a></li>
                         </ul>
                     </li>
-                    <li><a href="#/social-media/"><span class="social_icon"></span>Redes Sociales</a></li>
+                    <li><a href="#/social-media/"><span class="social_icon"></span>Redes Sociales</a></li>  
                 </ul>
             </nav>
         
